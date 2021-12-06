8575e59a58851e011a3219d8671957a5a256d15b
ypiard85 <60528207+ypiard85@users.noreply.github.com>
2021-12-06 12:33:06 +0100

    arrow fonction

--- 2-functions/script.js
+++ 2-functions/script.js
@@ -1,13 +1,13 @@
 //2.1 Fonctions simples
-const sayHello = function() {
+const sayHello = () => {
     console.log('Hello')
 }
 
-const sayMyName = function(first, last) {
+const sayMyName = (first, last) => {
     console.log(first, last)
 }
 
-const sayMyAge = function(age) {
+const sayMyAge = (age) => {
     console.log('You are ' + age + ' years old')
 }
 
@@ -22,12 +22,12 @@ const object = {
     color: 'red',
     shape: 'circle',
     threeDimensions: false,
-    showThis: function() {
+    showThis: () => {
         console.log(this)
     }
 }
 
-object.showThis()
+object.showThis() //Permet d'afficher la fonction showThis
 
 //----------------------//
 //2.3 Application
@@ -38,11 +38,17 @@ const odile = {
         last: 'Crok'
     },
     age: 23,
-    //sayHello: ...
-    //sayMyName:...
-    //sayMyAge:...
+    sayHello: () => {
+        console.log('Hello')
+    },
+    sayMyName: (first, last) => {
+        console.log(first, last)
+    },
+    sayMyAge: (age) => {
+        console.log('You are ' + age + ' years old')
+    }
 }
 
-//odile.sayHello()
-//odile.sayMyName()
-//odile.sayMyAge()
+odile.sayHello()
+odile.sayMyName('yoann', 'piard')
+odile.sayMyAge(25)
